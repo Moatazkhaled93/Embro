dade1b196607a45c52508b7fef63b2726d1fe8d6
Moataz khaled <36565838+Moatazkhaled93@users.noreply.github.com>
2018-11-05 00:14:42 +0200

    Api sign up and login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -12,8 +12,7 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    { Schema::defaultStringLength (100);
     }
 
     /**
